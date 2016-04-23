7961aa379e4c1af0eba69637a9f92d2a497056c9
esimkowitz <esimkowitz@wustl.edu>
2016-04-23 18:01:20 -0500

    Add a couple, delete a couple
    Some comments were added, others were moved to the README file.

--- ArduinoCAN SD/ArduinoCAN SD.ino
+++ ArduinoCAN SD/ArduinoCAN SD.ino
@@ -2,26 +2,21 @@
 ArduinoCan_SD.ino
 
 WURacing Electronics Team Spring 2016
+Carter Durno, Michael Greer, Thomas Kelly, Evan Simkowitz
 
 The purpose of this code is to read packets coming from our AEM Infinity ECU over the
-AEMNet protocol and write it over XBee to a computer in the Pit. Hardware-wise, we are 
-using an Arduino UNO, the SparkFun CAN-Bus Shield, a pair of XBee radios, and a 
-custom-made dashboard. The dashboard contains a bunch of LEDs for our RPM display, an
-OLED for whatever you want (we are using it to display the numeric RPM), and a check engine
-light. We are also using a custom connector that takes the four wires from
-the AEMNet connector and maps them to a DB9 connector to connect to our CAN-Bus Shield.
+AEMNet protocol and write it over XBee to a computer. 
 
 Be aware that to read the XBee stream, we wrote our own reader on the computer side which
 displays our data in real-time and writes it to a CSV. Because our ECU writes data over in 
 packets, we left most of the sorting to our Python application, which reads the data in 
 and organizes it before saving it.
 
-We have updated the code to store a backup of the data to a Micro-SD card in case the stream
-is disrupted. This requires no extra hardware as the SparkFun CAN-Bus Shield has a built-in
-Micro-SD card reader.
-
 Many of the libraries have been adapted to work with AEMNet, which varies slightly from the
 CAN-Bus standard.
+
+We have updated the code to store a backup of the data to a Micro-SD card in case the stream
+is disrupted.
 */
 
 
