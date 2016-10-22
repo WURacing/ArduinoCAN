75ed6a414910cc0eb453b36aadb37a97604d73a0
EthanSnow <enivi@DESKTOP-K0G0I5N.dhcp.wustl.edu>
2016-10-22 13:33:53 -0500

    Updated to read from 4 sensors

--- TMP006/TMP006.ino
+++ TMP006/TMP006.ino
@@ -31,26 +31,26 @@
 #include "I2C_16.h"
 #include "TMP006.h"
 
-uint8_t sensor1 = 0x40; // I2C address of TMP006, can be 0x40-0x47
+uint8_t sensorArray[] = {0x40, 0x41, 0x42, 0x43}; // Array of I2C addresses for TMP006 sensors (addresses can be 0x40-0x47)
 uint16_t samples = TMP006_CFG_16SAMPLE; // # of samples per reading, can be 1/2/4/8/16
 
 void setup()
 {
+  float objectTemps[4];
   Serial.begin(9600);
   Serial.println("TMP006 Example");
-
-  config_TMP006(sensor1, samples);
+  for (int i = 0, i < 4; i++) {
+    config_TMP006(sensorArray[i], samples);
+  }
 }
 
 void loop()
 {
-  float object_temp = readObjTempC(sensor1);
-  Serial.print("Object Temperature: "); 
-  Serial.print(object_temp); Serial.println("*C");
-
-  float sensor_temp = readDieTempC(sensor1);
-  Serial.print("Sensor Temperature: "); 
-  Serial.print(sensor_temp); Serial.println("*C");
-
+  //float object_temp = readObjTempC(sensor1);
+  for (int j = 0; j < 4; j++) {
+    objectTemps[j] = readObjectTempC(sensorArray[j])
+    Serial.print("Object " + j + "Temperature: "); 
+    Serial.print(objectTemps[j]); Serial.println("*C");
+  }
   delay(2000); // delay 1 second for every 4 samples per reading
 }
